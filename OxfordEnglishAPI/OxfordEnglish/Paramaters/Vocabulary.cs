2c8025c0161b372b8dbf200357f48a5b99683249
Tan loc <johndoe@example.com>
2018-01-13 22:04:35 +0700

    create folder

---
+++ OxfordEnglishAPI/OxfordEnglish/Paramaters/Vocabulary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OxfordEnglish.Paramaters
+{
+    public class Vocabulary
+    {
+        public int Id { get; set; }
+        public string VocabularyName { get; set; }
+    }
+}
